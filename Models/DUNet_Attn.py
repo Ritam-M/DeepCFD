ee01c25ec73b5c8f87e0c989747ee6bdeca2cdc1
Ritam  Majumdar <hrickmajumdar@gmail.com>
2021-08-25 14:20:53 +0530

    Update DUNet_Attn.py

--- Models/DUNet_Attn.py
+++ Models/DUNet_Attn.py
@@ -1,1 +1,70 @@
+class Attention_block(nn.Module):
+    def __init__(self,F_g,F_l,F_int):
+        super(Attention_block,self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            # nn.BatchNorm2d(F_int)
+            )
+        
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            # nn.BatchNorm2d(F_int)
+        )
 
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1,stride=1,padding=0,bias=True),
+            # nn.BatchNorm2d(1),
+            # nn.Sigmoid()
+        )
+        
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self,g,x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1+x1)
+        psi = self.psi(psi)
+
+        return x*psi
+      
+ class Att_UNet(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, filters=[16, 32, 64], layers=2,
+                 weight_norm=True, batch_norm=True, activation=nn.ReLU, final_activation=None):
+        super().__init__()
+        assert len(filters) > 0
+        self.final_activation = final_activation
+        self.encoder = create_encoder(in_channels, filters, kernel_size, weight_norm, batch_norm, activation, layers)
+        self.decoder = create_decoder(out_channels, filters, kernel_size, weight_norm, batch_norm, activation, layers)
+        self.Attention = create_attention(out_channels, filters, kernel_size, weight_norm, batch_norm, activation, layers)
+        
+    def encode(self, x):
+        tensors = []
+        indices = []
+        sizes = []
+        for encoder in self.encoder:
+            x = encoder(x)
+            sizes.append(x.size())
+            tensors.append(x)
+            x, ind = F.max_pool2d(x, 2, 2, return_indices=True)
+            indices.append(ind)
+        return x, tensors, indices, sizes
+
+    def decode(self, x, tensors, indices, sizes):
+        
+        for decoder, attn in zip(self.decoder, self.Attention):
+            tensor = tensors.pop()
+            size = sizes.pop()
+            ind = indices.pop()
+            x = F.max_unpool2d(x, ind, 2, 2, output_size=size)
+            x = attn(tensor,x)
+            x = torch.cat([tensor, x], dim=1)
+            x = decoder(x)
+        return x
+
+    def forward(self, x):
+        x, tensors, indices, sizes = self.encode(x)
+        #print(x.shape)
+        x = self.decode(x, tensors, indices, sizes)
+        if self.final_activation is not None:
+            x = self.final_activation(x)
+        return x
